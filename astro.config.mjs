5d59bb63531c5e7cda3ffed4d65ed86389027ed9
Girlfierce <girlfierce@users.noreply.github.com>
2026-05-20 18:27:29 +0530

    Add GitHub Actions workflow for automatic deployment to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://girlfierce.github.io',
+  base: '/astro-wiki',
   integrations: [tailwind(), react()],
   markdown: {
     shikiConfig: {
